ad80b96b569cc5e45426426fbe5cef13f06055d1
Filip Jukic <404@selion.at>
2025-05-14 09:45:19 +0200

    Added Flash Plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -22,6 +22,17 @@ vim.keymap.set('n', 'grq', vim.lsp.buf.hover, { desc = 'Show Documentation' })
 
 return {
   {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode= { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+  },
+  {
     'nvim-tree/nvim-tree.lua',
     config = function()
       require('nvim-tree').setup {
